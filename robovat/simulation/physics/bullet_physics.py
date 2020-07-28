716d7dc3f000220de2d2c47b0b5070e566cd0343
Michael Lingelbach <m.j.lbach@gmail.com>
2020-07-27 17:42:54 -0700

    Cleanups

--- robovat/simulation/physics/bullet_physics.py
+++ robovat/simulation/physics/bullet_physics.py
@@ -142,7 +142,8 @@ class BulletPhysics(physics.Physics):
                  filename,
                  pose,
                  scale=1.0,
-                 is_static=False):
+                 is_static=False,
+                 ):
         """Load a body into the simulation.
 
         Args:
@@ -167,7 +168,7 @@ class BulletPhysics(physics.Physics):
         if ext == '.urdf':
             # Do not use pybullet.URDF_USE_SELF_COLLISION since it will cause
             # problems for the motor control in Bullet.
-            # pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_RENDERING, 0)
+            pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_RENDERING, 0)
             body_uid = pybullet.loadURDF(
                 fileName=filename,
                 basePosition=position,
@@ -177,7 +178,7 @@ class BulletPhysics(physics.Physics):
                 physicsClientId=self.uid,
                 flags=pybullet.URDF_USE_SELF_COLLISION_EXCLUDE_PARENT,
                 )
-            # pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_RENDERING, 1)
+            pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_RENDERING, 1)
         else:
             raise ValueError('Unrecognized extension %s.' % ext)
 
